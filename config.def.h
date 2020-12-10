97c44683e5d9972e91abdee6da2a00eaa5de283b
Manuel Díaz <yosoymacnolo@gmail.com>
2020-12-09 20:13:06 -0600

    Changing theme

--- config.def.h
+++ config.def.h
@@ -3,24 +3,26 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 5;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=11" };
-static const char dmenufont[]       = "Hack Nerd Font:size=11";
+static const char *fonts[]          = { "Open Sans:size=11:antialias=true:autohint=true",
+                                        "Hack Nerd Font:size=14:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Open Sans:size=11.5:antialisa=true:autohint=true";
+static const char col_gray0[]       = "#121212";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray3[]       = "#bebebe";
+static const char col_gray4[]       = "#efefef";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray0, col_gray0 },
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "﬐", "" };
+static const char *tags[] = { "  ", "  ", "  ", "  ", "  ", "  ", "  ", " ﬐ ", "  " };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -57,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray0, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
