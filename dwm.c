9caff9e8be17a1ab845c4527095b4cfe22b71b28
Manuel Díaz <yosoymacnolo@gmail.com>
2020-12-25 19:20:42 -0600

    Fixing JAVA AWT/Swing and modifying smart gaps

--- dwm.c
+++ dwm.c
@@ -1326,8 +1326,8 @@ resizeclient(Client *c, int x, int y, int w, int h)
 	if (c->isfloating || c->mon->lt[c->mon->sellt]->arrange == NULL) {
 		gapincr = gapoffset = 0;
 	} else {
-		/* Remove border and gap if layout is monocle or only one client */
-		if (c->mon->lt[c->mon->sellt]->arrange == monocle || n == 1) {
+		// Remove border and gap if layout is monocle or only one client 
+		if (c->mon->lt[c->mon->sellt]->arrange == monocle) {//|| n == 1) {
 			gapoffset = 0;
 			gapincr = -2 * borderpx;
 			wc.border_width = 0;
