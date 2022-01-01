b222c70b30fedd6c64f825ec61b048fb21a175eb
Manuel Diaz <ceo@macnolo.net>
2021-12-31 22:46:53 -0600

    Disable tag autohiding

--- dwm.c
+++ dwm.c
@@ -882,9 +882,9 @@ drawbar(Monitor *m)
 	}
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
-		/* do not draw vacant tags */
+		/* do not draw vacant tags
 		if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
-		continue;
+		continue;*/
 
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
